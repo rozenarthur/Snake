43ca2739022ada048e55e646e452c9d456b4217e
Arthur Rozenberg <rozenarthur@gmail.com>
2017-06-17 15:44:23 -0400

    add a snake head sprite for the head of the snake with rotations

--- test2/Main.py
+++ test2/Main.py
@@ -12,32 +12,60 @@ gameDisplay = pygame.display.set_mode((displayWidth, displayHeight))
 
 pygame.display.set_caption("Slytherin'")
 
-clock = pygame.time.Clock()
-fps = 30
+img = pygame.image.load('head.png')
 
+clock = pygame.time.Clock()
+fps = 25
+
+direction = "right"
 font = pygame.font.SysFont(None, 25)
 
-#modify the snake character, ie make him bigger when he eats an apple
+#modify the snake character, i.e. make him bigger when he eats an apple
 def snake(blockSize, snakelist):
-    for XnY in snakelist:
+
+    if direction == "right":
+        head = pygame.transform.rotate(img, 270)
+
+    if direction == "left":
+        head = pygame.transform.rotate(img, 90)
+
+    if direction == "up":
+        head = img
+
+    if direction == "down":
+        head = pygame.transform.rotate(img, 180)
+
+
+    gameDisplay.blit(head, (snakelist[-1][0],snakelist[-1][1]))
+
+    for XnY in snakelist[:-1]:
         pygame.draw.rect(gameDisplay, blue, [XnY[0], XnY[1], blockSize, blockSize])
 
+def text_objects(text, color):
+    textSurface = font.render(text, True, color)
+    return textSurface, textSurface.get_rect()
+
 def msg_to_scrn(msg, color):
-    screen_text = font.render(msg, True, color)
-    gameDisplay.blit(screen_text, [displayWidth/2, displayHeight/2])
+
+    # screen_text = font.render(msg, True, color)
+    # gameDisplay.blit(screen_text, [displayWidth/2, displayHeight/2])
+    textSurf, textRect = text_objects(msg, color)
+    textRect.center = (displayWidth/2), (displayHeight/2)
+    gameDisplay.blit(textSurf, textRect)
 
 
 blockSize = 20  # size of each square of the snake
 
 # **Logical Game loop that runs the controls and changes of your game**
 def gameLoop():
+    global direction
     gameExit = False
     gameOver = False
 
     lead_x = displayWidth / 2
     lead_y = displayHeight / 2
 
-    lead_x_change = 0
+    lead_x_change = 10
     lead_y_change = 0
 
     snakeList = []
@@ -70,18 +98,22 @@ def gameLoop():
                 if event.key == pygame.K_LEFT:  # if the left arrow was clicked
                     lead_x_change = -blockSize
                     lead_y_change = 0  # avoids diagonal movement
+                    direction = "left"
 
                 elif event.key == pygame.K_RIGHT:  # if the right arrow was clicked
                     lead_x_change = blockSize
                     lead_y_change = 0 # avoids diagonal movement
+                    direction = "right"
 
                 elif event.key == pygame.K_UP:  # if the up arrow was clicked
                     lead_y_change = -blockSize
                     lead_x_change = 0 # avoids diagonal movement
+                    direction = "up"
 
                 elif event.key == pygame.K_DOWN:  # if the down arrow was clicked
                     lead_y_change = blockSize
                     lead_x_change = 0 # avoids diagonal movement
+                    direction = "down"
 
             #  **Stops Movements when the button click is over (EVENT KEYUP), THIS IS AN EXAMPLE, NOT USEFUL FOR SNAKE GAME**
             # if event.type == pygame.KEYUP:
@@ -124,8 +156,8 @@ def gameLoop():
         #
         #         snakeLength += 1
 
-        if lead_x > randAppleX and lead_x < randAppleX + blockSize or lead_x + blockSize > randAppleX and lead_x + blockSize < randAppleX + blockSize:
-            if lead_y > randAppleY and lead_y < randAppleY + blockSize or lead_y + blockSize > randAppleY and lead_y + blockSize < randAppleY + blockSize:
+        if lead_x >= randAppleX and lead_x <= randAppleX + blockSize or lead_x + blockSize >= randAppleX and lead_x + blockSize <= randAppleX + blockSize:
+            if lead_y >= randAppleY and lead_y <=randAppleY + blockSize or lead_y + blockSize >= randAppleY and lead_y + blockSize <= randAppleY + blockSize:
 
                 randAppleX = round(random.randrange(0,displayWidth - blockSize))  # / 10.0) * 10.0  # so apple not offscreen and only appears in multiples of 10
                 randAppleY = round(random.randrange(0,displayHeight - blockSize))# / 10.0) * 10.0  # so apple not offscreen and only appears in multiples of 10
